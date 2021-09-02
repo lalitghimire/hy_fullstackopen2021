cef093d0b21bce1ab1d61e908215e45f00ba1c94
Lalit Ghimire <lalitghimire@kamk.fi>
2021-09-02 17:02:54 +0300

    completed unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+//button component with click event
 const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
@@ -9,15 +10,19 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const clickGood = () => {
     setGood(good + 1);
+    setTotal(total + 1);
   };
   const clickBad = () => {
     setBad(bad + 1);
+    setTotal(total + 1);
   };
   const clickNeutral = () => {
     setNeutral(neutral + 1);
+    setTotal(total + 1);
   };
 
   return (
@@ -31,6 +36,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average: {(good - bad) / total}</p>
+      <p>positive: {(good / total) * 100} %</p>
     </>
   );
 };
