c066d680ee1fcea428b72122817d621fb0b06ee8
Lalit Ghimire <lalitghimire@kamk.fi>
2021-09-02 18:27:38 +0300

    completed unicafe step 5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,20 +5,32 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  );
+};
+
 // Statistics component
-const Statistics = ({ good, bad, neutral, total }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100 + " %";
+
   // To display statistics only if feedback given
   if (!total) {
     return <p>No feedback given</p>;
   }
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average: {(good - bad) / total}</p>
-      <p>positive: {(good / total) * 100} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </div>
   );
 };
@@ -29,20 +41,16 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
 
   // functions which increase value of good, bad, neutral and total
   const clickGood = () => {
     setGood(good + 1);
-    setTotal(total + 1);
   };
   const clickBad = () => {
     setBad(bad + 1);
-    setTotal(total + 1);
   };
   const clickNeutral = () => {
     setNeutral(neutral + 1);
-    setTotal(total + 1);
   };
 
   return (
@@ -53,7 +61,7 @@ const App = () => {
       <Button handleClick={clickNeutral} text="neutral" />
       <Button handleClick={clickBad} text="bad" />
       <h1>statistics</h1>
-      <Statistics good={good} bad={bad} neutral={neutral} total={total} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
