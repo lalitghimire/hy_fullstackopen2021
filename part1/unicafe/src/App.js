5f239a655323b153c7fee3177d46181929269d1f
Lalit Ghimire <lalitghimire@kamk.fi>
2021-09-02 15:59:22 +0300

    completed unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,38 @@
-import React, { useState } from "react-dom";
+import React, { useState } from "react";
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
 const App = () => {
   // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return <div> Hello world</div>;
+  const clickGood = () => {
+    setGood(good + 1);
+  };
+  const clickBad = () => {
+    setBad(bad + 1);
+  };
+  const clickNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  return (
+    <>
+      <h1> give feedback</h1>
+
+      <Button handleClick={clickGood} text="good" />
+      <Button handleClick={clickNeutral} text="neutral" />
+      <Button handleClick={clickBad} text="bad" />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
 };
 
 export default App;
