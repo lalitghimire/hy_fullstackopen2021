02623359afc5649603e26315d5f65f0296002247
Lalit Ghimire <lalitghimire@kamk.fi>
2021-09-16 13:20:00 +0300

    added working solution

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -3,21 +3,61 @@ import axios from "axios";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  console.log(countries);
+  const [searchName, setSearchName] = useState("");
+
   useEffect(() => {
     console.log("effect");
     axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
       console.log("promise fulfilled");
-
+      console.log(response.data);
       setCountries(response.data);
     });
   }, []);
 
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+    setSearchName(event.target.value);
+  };
+
+  const showCountriesList = (searchName, countries) => {
+    const countriesList = countries.filter((country) =>
+      country.name.toLowerCase().includes(searchName.toLowerCase())
+    );
+    console.log(countriesList);
+    if (searchName) {
+      if (countriesList.length > 10) {
+        return <p>Too many matches, specify another filter</p>;
+      } else if (countriesList.length > 1) {
+        return countriesList.map((c) => <p key={c.name}>{c.name} </p>);
+      } else if (countriesList.length === 1) {
+        let country = countriesList[0];
+        return (
+          <div>
+            <h2>{country.name}</h2>
+
+            <p>capital {country.capital} </p>
+            <p>population {country.population} </p>
+
+            <h3> languages </h3>
+
+            {country.languages.map((c) => (
+              <p key={c.name}>{c.name}</p>
+            ))}
+
+            {console.log(countriesList[0].languages[0])}
+            <img src={country.flag} alt="flag" width="100" height="100" />
+          </div>
+        );
+      }
+    }
+  };
+
   return (
     <div>
-      {countries.map((country) => (
-        <p key={country.name}> {country.name}</p>
-      ))}
+      find countries <input value={searchName} onChange={handleSearch} />
+      <br />
+      <br />
+      {showCountriesList(searchName, countries)}
     </div>
   );
 };
