a584d14a41a60e1d18b4e4aa725c6d4109b350f5
Lalit Ghimire <lalitghimire@kamk.fi>
2021-09-23 23:25:25 +0300

    completed step1-4(3.1-3.4)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,66 @@
 const express = require("express");
 const app = express();
 
+// persons object
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+// routes
+//route to home
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello from homepage");
+});
+
+// route to info
+app.get("/info", (req, res) => {
+  let dateNow = Date(Date.now());
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p> 
+      <p> ${dateNow}</p>`
+  );
+});
+
+// route which return the list of persons
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+// route to get a single person from list
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.send(person);
+  } else {
+    res.status(404).send("Person not found");
+  }
+});
+
+// delete entry
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end;
 });
 
 const PORT = 3001;
