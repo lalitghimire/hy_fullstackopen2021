67cac2c8ecf244018c82cbc3ed37a41fc584a0c0
Lalit Ghimire <lalitghimire@kamk.fi>
2021-09-23 22:18:24 +0300

    created simple express server

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
