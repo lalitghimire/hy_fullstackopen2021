c47c3b2896a90389f18f5cea33908e912e772e51
Lalit Ghimire <lalitghimire@kamk.fi>
2021-09-26 20:11:45 +0300

    added procfile

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -93,7 +93,7 @@ app.post("/api/persons", (req, res) => {
 });
 
 // create server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
 });
