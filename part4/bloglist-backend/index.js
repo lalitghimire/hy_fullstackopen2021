c95b02924da67ad4f614e941b04e73d2413bbff5
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-09 16:39:36 +0300

    completed 4.2

--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -1,25 +1,9 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
-const blogRouter = require('./controllers/blogs')
 
-mongoose
-    .connect(config.MONGODB_URI)
-    .then(() => {
-        console.log('connected to database')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB')
-    })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)
 })
