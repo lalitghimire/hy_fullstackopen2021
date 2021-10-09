f70d4dd7fda1f8c6f5eb227fb75ea80001715240
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-09 15:52:14 +0300

    refactored

--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -1,33 +1,25 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl).then(() => {
-    console.log('connected to database')
-})
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        console.log('connected to database')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB')
+    })
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogRouter)
 
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
