a15bbea0fbafe86a1248d28ae4d67286a410226f
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-12 22:18:08 +0300

    added new test configuration

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const total = blogs.reduce(
+        (total, likesCount) => total + likesCount.likes,
+        0
+    )
+    return total
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
