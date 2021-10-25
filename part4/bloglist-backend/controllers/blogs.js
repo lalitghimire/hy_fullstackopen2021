f455766fa9e25722e026cb63ba36a830ecebaf6f
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-25 18:02:22 +0300

    added update functionality

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -1,11 +1,13 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
+// route to get all blogs
 blogRouter.get('/', async (req, res) => {
     const blogs = await Blog.find({})
     res.json(blogs.map((blog) => blog.toJSON()))
 })
 
+// route to post a new blog
 blogRouter.post('/', async (req, res, next) => {
     const blog = new Blog(req.body)
 
@@ -17,10 +19,30 @@ blogRouter.post('/', async (req, res, next) => {
     }
 })
 
+// route to delete a post
 blogRouter.delete('/:id', async (req, res, next) => {
     try {
         await Blog.findByIdAndRemove(req.params.id)
         res.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+// route to update a post with changed parameter
+blogRouter.put('/:id', async (req, res, next) => {
+    const blog = req.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+    try {
+        let editedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+            new: true,
+        })
+        res.json(editedBlog.toJSON())
     } catch (exception) {
         next(exception)
     }
