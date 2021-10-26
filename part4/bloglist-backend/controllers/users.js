24e3857cf2e84fef2519aa858f0b4138f53ae370
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-26 23:09:31 +0300

    completed 4.15

--- part4/bloglist-backend/controllers/users.js
+++ part4/bloglist-backend/controllers/users.js
@@ -1,7 +1,28 @@
+const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
+const User = require('../models/user')
 
-userRouter.get('/', (req, res) => {
-    res.send('<h1> Hello </h1>')
+// create new users using POST method
+userRouter.post('/', async (req, res) => {
+    const body = req.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+    res.json(savedUser)
+})
+
+// implementation to see the details of all users
+userRouter.get('/', async (req, res) => {
+    const users = await User.find({})
+    res.json(users.map((user) => user.toJSON()))
 })
 
 module.exports = userRouter
