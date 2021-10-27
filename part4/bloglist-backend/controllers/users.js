97a90013205583b07e8138d6e41876bb1e565a1f
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-27 21:14:15 +0300

    completed 4.16

--- part4/bloglist-backend/controllers/users.js
+++ part4/bloglist-backend/controllers/users.js
@@ -3,9 +3,14 @@ const userRouter = require('express').Router()
 const User = require('../models/user')
 
 // create new users using POST method
-userRouter.post('/', async (req, res) => {
+userRouter.post('/', async (req, res, next) => {
     const body = req.body
 
+    if (body.password.length < 3) {
+        return res
+            .status(400)
+            .json({ error: 'password should be at least 3 characters' })
+    }
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
@@ -14,9 +19,12 @@ userRouter.post('/', async (req, res) => {
         name: body.name,
         passwordHash,
     })
-
-    const savedUser = await user.save()
-    res.json(savedUser)
+    try {
+        const savedUser = await user.save()
+        res.json(savedUser)
+    } catch (exception) {
+        next(exception)
+    }
 })
 
 // implementation to see the details of all users
