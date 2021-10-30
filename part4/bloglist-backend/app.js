053533ca569af12280773528c2395fdce32e00d3
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-30 18:11:15 +0300

    completed 4.18

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -7,6 +7,7 @@ const userRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to database')
 
@@ -27,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
