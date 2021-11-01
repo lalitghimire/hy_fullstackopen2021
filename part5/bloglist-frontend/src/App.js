33de1a07be4bcd5768db7391e3ea2eeecc75b289
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-01 19:38:07 +0200

    added login form

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,22 +3,49 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs))
+    }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+    const handleLogin = (event) => {
+        event.preventDefault()
+        console.log('logging in with ', username, password)
+    }
+
+    return (
+        <div>
+            <h1>blogs</h1>
+            <form onSubmit={handleLogin}>
+                <div>
+                    username
+                    <input
+                        type='text'
+                        value={username}
+                        name='Username'
+                        onChange={({ target }) => setUsername(target.value)}
+                    />
+                </div>
+                <div>
+                    password
+                    <input
+                        type='password'
+                        value={password}
+                        name='Password'
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                </div>
+                <button type='submit'>login</button>
+            </form>
+
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    )
 }
 
 export default App
