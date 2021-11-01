8f1ab9c2bcfe5a45c6ff7f34628286dea57596e9
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-01 19:37:26 +0200

    refactored blogservice

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 const baseUrl = '/api/blogs'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then((response) => response.data)
 }
 
-export default { getAll }
+const blogService = { getAll }
+export default blogService
