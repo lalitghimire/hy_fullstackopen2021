0fcf74ce7d0c47d42544543a824c84bd8f82445e
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-01 21:57:32 +0200

    added loginservice

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
 
     useEffect(() => {
         blogService.getAll().then((blogs) => setBlogs(blogs))
     }, [])
 
-    const handleLogin = (event) => {
+    const handleLogin = async (event) => {
         event.preventDefault()
         console.log('logging in with ', username, password)
+
+        try {
+            const user = await loginService.login({ username, password })
+            setUser(user)
+
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage('Wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
     }
 
     return (
