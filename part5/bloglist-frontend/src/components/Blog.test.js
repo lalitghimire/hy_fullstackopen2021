cc75d3301f950c7f22eb9c9144d7205a3a3c104f
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-09 19:32:44 +0200

    completed 5.13

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders blog', () => {
+test(`renders blog's title and author`, () => {
     const blog = {
         title: 'this is blog render testing',
         author: 'tester',
