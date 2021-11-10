532dcedfe014f22d07a21207f94d3cd46c05c9ef
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-10 19:20:53 +0200

    completed 5.17-5.18(+bonus ex)

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,34 @@
 describe('blog app', function () {
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const testuser = {
+            name: 'Lalit Ghimire',
+            username: 'lalit',
+            password: 'password',
+        }
+        cy.request('POST', 'http://localhost:3003/api/users', testuser)
         cy.visit('http://localhost:3000')
     })
     it('login form is shown', function () {
         cy.get('#loginform-username')
         cy.get('#loginform-password')
+    })
+
+    describe('Login', function () {
+        it('login successful with correct credentials', function () {
+            cy.get('#loginform-username').type('lalit')
+            cy.get('#loginform-password').type('password')
+            cy.get('#login-btn').click()
+
+            cy.contains('Lalit Ghimire is logged in')
+        })
+        it('login fails with wrong credentials', function () {
+            cy.get('#loginform-username').type('lalit')
+            cy.get('#loginform-password').type('wrongpassword')
+            cy.get('#login-btn').click()
+
+            cy.get('.error').should('contain', 'Wrong credentials provided')
+            cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+        })
     })
 })
