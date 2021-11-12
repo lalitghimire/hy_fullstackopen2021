00e9c94aebf0bc8efc5e2bce69f346775f4514f7
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-12 20:43:17 +0200

    completed 6.1-6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -21,13 +21,13 @@ const App = () => {
 
     return (
         <div>
-            {/* <button onClick={good}>good</button>
+            <button onClick={good}>good</button>
             <button onClick={ok}>ok</button>
             <button onClick={bad}>bad</button>
             <button onClick={reset}>reset stats</button>
             <div>good {store.getState().good}</div>
             <div>ok {store.getState().ok}</div>
-            <div>bad {store.getState().bad}</div> */}
+            <div>bad {store.getState().bad}</div>
         </div>
     )
 }
