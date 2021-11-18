bfb73a4f0829ffc5f079348b718d6a4a7eb3c29f
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-18 12:35:38 +0200

    added filter reducer

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,10 +2,12 @@ import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { createStore, combineReducers } from 'redux'
+import filterReducer from './reducers/filterReducer'
 
 const reducer = combineReducers({
     anecdotes: anecdoteReducer,
     notification: notificationReducer,
+    filter: filterReducer,
 })
 
 const store = createStore(reducer, composeWithDevTools())
