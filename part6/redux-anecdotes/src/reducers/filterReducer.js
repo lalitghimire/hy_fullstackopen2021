4588706be347d6f44894325b750b03a09ee2859b
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-18 12:34:18 +0200

    added filter reducer

---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,13 @@
+const initialFilter = ''
+
+const notificationReducer = (state = initialFilter, action) => {
+    switch (action.type) {
+        case 'FILTER':
+            return action.data
+
+        default:
+            return state
+    }
+}
+
+export default notificationReducer
