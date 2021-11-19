31f1761b11fbf0cf1c59830038f85b54039991fd
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-19 16:19:46 +0200

    added create service

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -7,5 +7,11 @@ const getAll = async () => {
     return response.data
 }
 
-const anecdoteService = { getAll }
+const create = async (content) => {
+    const anecdoteObject = { content: content, vote: 0 }
+    const response = await axios.post(baseUrl, anecdoteObject)
+    return response.data
+}
+
+const anecdoteService = { getAll, create }
 export default anecdoteService
