3d650b6ff7dfafee019e51f046b14b0f66bc5e9b
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-22 10:58:59 +0200

    rafactor to old redux(complete 6.19)

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+//import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-    const notification = useSelector((state) => state.notification)
+const Notification = ({ notification }) => {
+    //const notification = useSelector((state) => state.notification)
+
     const style = {
         border: 'solid',
         fontSize: '30px',
@@ -16,4 +18,10 @@ const Notification = () => {
     return <div style={style}>{notification}</div>
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+    return {
+        notification: state.notification,
+    }
+}
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
