2fd30124e4e09d798c6f0607cbee9ed0ee109a48
Lalit Ghimire <lalitghimire@gmail.com>
2021-11-26 18:38:35 +0200

    add view button

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,5 +1,10 @@
 import React from 'react'
 const Blog = ({ blog }) => {
+    console.log('here is the blog object', blog)
+
+    const viewClick = () => {
+        return <div>{blog.url} </div>
+    }
     const blogStyle = {
         paddingTop: 10,
         paddingLeft: 2,
@@ -10,7 +15,8 @@ const Blog = ({ blog }) => {
     }
     return (
         <div className='blog' style={blogStyle}>
-            {blog.title} {blog.author}
+            {blog.title} {blog.author}{' '}
+            <button onClick={viewClick}>view </button>
         </div>
     )
 }
