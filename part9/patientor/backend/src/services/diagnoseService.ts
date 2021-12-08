cc48953f07d21b3f8357fe779aa71e28b17f064f
Lalit Ghimire <lalitghimire@gmail.com>
2021-12-08 20:06:30 +0200

    complete 9.9-9.10

---
+++ part9/patientor/backend/src/services/diagnoseService.ts
@@ -0,0 +1,9 @@
+import diagnoses from '../data/diagnoses';
+
+import { Diagnose } from '../types';
+
+const getDiagnoses = (): Array<Diagnose> => {
+    return diagnoses;
+};
+
+export default { getDiagnoses };
