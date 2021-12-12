b100cbcd7b7a011a160bc233600b27966d5b122e
Lalit Ghimire <lalitghimire@gmail.com>
2021-12-12 23:59:29 +0200

    add page for single patient

--- part9/patientor/patientor/src/App.tsx
+++ part9/patientor/patientor/src/App.tsx
@@ -1,50 +1,57 @@
-import React from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { Button, Divider, Header, Container } from "semantic-ui-react";
+import React from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Button, Divider, Header, Container } from 'semantic-ui-react';
 
-import { apiBaseUrl } from "./constants";
-import { useStateValue } from "./state";
-import { Patient } from "./types";
+import { apiBaseUrl } from './constants';
+import { useStateValue } from './state';
+import { Patient } from './types';
 
-import PatientListPage from "./PatientListPage";
+import PatientListPage from './PatientListPage';
+import SinglePatientPage from './SinglePatientPage';
 
 const App = () => {
-  const [, dispatch] = useStateValue();
-  React.useEffect(() => {
-    void axios.get<void>(`${apiBaseUrl}/ping`);
+    const [, dispatch] = useStateValue();
+    React.useEffect(() => {
+        void axios.get<void>(`${apiBaseUrl}/ping`);
 
-    const fetchPatientList = async () => {
-      try {
-        const { data: patientListFromApi } = await axios.get<Patient[]>(
-          `${apiBaseUrl}/patients`
-        );
-        dispatch({ type: "SET_PATIENT_LIST", payload: patientListFromApi });
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    void fetchPatientList();
-  }, [dispatch]);
+        const fetchPatientList = async () => {
+            try {
+                const { data: patientListFromApi } = await axios.get<Patient[]>(
+                    `${apiBaseUrl}/patients`
+                );
+                dispatch({
+                    type: 'SET_PATIENT_LIST',
+                    payload: patientListFromApi,
+                });
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        void fetchPatientList();
+    }, [dispatch]);
 
-  return (
-    <div className="App">
-      <Router>
-        <Container>
-          <Header as="h1">Patientor</Header>
-          <Button as={Link} to="/" primary>
-            Home
-          </Button>
-          <Divider hidden />
-          <Switch>
-            <Route path="/">
-              <PatientListPage />
-            </Route>
-          </Switch>
-        </Container>
-      </Router>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <Container>
+                    <Header as='h1'>Patientor</Header>
+                    <Button as={Link} to='/' primary>
+                        Home
+                    </Button>
+                    <Divider hidden />
+                    <Switch>
+                        <Route path='/singlepatient'>
+                            <SinglePatientPage />
+                        </Route>
+                        <Route path='/'>
+                            <PatientListPage />
+                        </Route>
+                    </Switch>
+                </Container>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
---
+++ part9/patientor/patientor/src/SinglePatientPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SinglePatientPage = () => {
+    return <div> hello from single patient page </div>;
+};
+
+export default SinglePatientPage;
