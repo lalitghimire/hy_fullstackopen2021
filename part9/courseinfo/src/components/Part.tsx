7f0161bd825308a45f8d927fce604aa9a3c5bac4
Lalit Ghimire <lalitghimire@gmail.com>
2021-12-12 22:20:47 +0200

    fix layout

--- part9/courseinfo/src/components/Part.tsx
+++ part9/courseinfo/src/components/Part.tsx
@@ -15,49 +15,53 @@ const Part = ({ part }: { part: CoursePart }) => {
         case 'normal':
             return (
                 <div>
-                    {' '}
-                    <b>
-                        {part.name} {part.exerciseCount}
-                    </b>{' '}
-                    <br />
-                    <i>{part.description} </i>
+                    <p>
+                        <b>
+                            {part.name} {part.exerciseCount}
+                        </b>
+                        <br />
+                        <i>{part.description} </i>
+                    </p>
                 </div>
             );
 
         case 'groupProject':
             return (
                 <div>
-                    {' '}
-                    <b>
-                        {part.name} {part.exerciseCount}
-                    </b>{' '}
-                    <br />
-                    project exercises{part.groupProjectCount}
+                    <p>
+                        <b>
+                            {part.name} {part.exerciseCount}
+                        </b>
+                        <br />
+                        project exercises {part.groupProjectCount}
+                    </p>
                 </div>
             );
 
         case 'submission':
             return (
                 <div>
-                    {' '}
-                    <b>
-                        {part.name} {part.exerciseCount}
-                    </b>{' '}
-                    <br />
-                    <i>{part.description} </i> <br />
-                    submit to {part.exerciseSubmissionLink}
+                    <p>
+                        <b>
+                            {part.name} {part.exerciseCount}
+                        </b>
+                        <br />
+                        <i>{part.description} </i> <br />
+                        submit to {part.exerciseSubmissionLink}
+                    </p>
                 </div>
             );
         case 'special':
             return (
                 <div>
-                    {' '}
-                    <b>
-                        {part.name} {part.exerciseCount}
-                    </b>{' '}
-                    <br />
-                    <i>{part.description} </i> <br />
-                    required skills: {part.requirements}
+                    <p>
+                        <b>
+                            {part.name} {part.exerciseCount}
+                        </b>
+                        <br />
+                        <i>{part.description} </i> <br />
+                        required skills: {part.requirements.toString()}
+                    </p>
                 </div>
             );
         default:
