e574354df4b3d4645ef086d7b95aaa67589c8ed0
Lalit Ghimire <lalitghimire@gmail.com>
2021-12-19 19:40:27 +0200

    add card style for  entry type

--- part9/patientor/patientor/src/components/EntryDetails.tsx
+++ part9/patientor/patientor/src/components/EntryDetails.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useStateValue } from '../state';
-import { Entry } from '../types';
+import {
+    Entry,
+    HealthCheckEntry,
+    HospitalEntry,
+    OccupationalHealthcareEntry,
+} from '../types';
+import { Icon, Card } from 'semantic-ui-react';
 
-const EntryDetails = ({ entry }: { entry: Entry }) => {
-    console.log('props for entrydetails', entry);
+// Hospital entry
+const HospitalEntryDetails = ({ entry }: { entry: HospitalEntry }) => {
     const [{ diagnosisDetail }] = useStateValue();
 
-    console.log('diagnoseis', diagnosisDetail);
     const showDetailsOfCode = (x: string) => {
         if (diagnosisDetail) {
             return diagnosisDetail[x].name;
@@ -15,20 +20,145 @@ const EntryDetails = ({ entry }: { entry: Entry }) => {
 
     return (
         <div>
-            {entry.date} {entry.description}
-            <ul>
-                {entry.diagnosisCodes?.map((x) => {
-                    console.log('code is here', x);
+            <Card fluid style={{ marginBottom: '10px' }}>
+                <Card.Content>
+                    <Card.Header>
+                        {entry.date} <Icon name='hospital'></Icon>
+                    </Card.Header>
+                    <Card.Meta>{entry.description}</Card.Meta>
 
-                    return (
-                        <li key={x}>
-                            {x} {showDetailsOfCode(x)}
-                        </li>
-                    );
-                })}
-            </ul>
+                    <Card.Description>
+                        <ul>
+                            {entry.diagnosisCodes?.map((x) => {
+                                return (
+                                    <li key={x}>
+                                        {x} {showDetailsOfCode(x)}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </Card.Description>
+                </Card.Content>
+            </Card>
         </div>
     );
+};
+
+// ocupationalHealthCareEntry
+const OccupationalHealthcareEntryDetails = ({
+    entry,
+}: {
+    entry: OccupationalHealthcareEntry;
+}) => {
+    const [{ diagnosisDetail }] = useStateValue();
+
+    const showDetailsOfCode = (x: string) => {
+        if (diagnosisDetail) {
+            return diagnosisDetail[x].name;
+        }
+    };
+
+    return (
+        <div>
+            <Card fluid style={{ marginBottom: '5px' }}>
+                <Card.Content>
+                    <Card.Header>
+                        {entry.date} <Icon name='stethoscope'></Icon>{' '}
+                        {entry.employerName}
+                    </Card.Header>
+                    <Card.Meta>{entry.description}</Card.Meta>
+
+                    <Card.Description>
+                        <ul>
+                            {entry.diagnosisCodes?.map((x) => {
+                                return (
+                                    <li key={x}>
+                                        {x} {showDetailsOfCode(x)}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </Card.Description>
+                </Card.Content>
+            </Card>
+        </div>
+    );
+};
+
+// HealthCheck Entry
+const HealthCheckEntryDetails = ({ entry }: { entry: HealthCheckEntry }) => {
+    const [{ diagnosisDetail }] = useStateValue();
+
+    const showDetailsOfCode = (x: string) => {
+        if (diagnosisDetail) {
+            return diagnosisDetail[x].name;
+        }
+    };
+
+    return (
+        <div>
+            <Card fluid style={{ marginBottom: '10px' }}>
+                <Card.Content>
+                    <Card.Header>
+                        {entry.date} <Icon name='doctor'></Icon>{' '}
+                    </Card.Header>
+                    <Card.Meta>{entry.description}</Card.Meta>
+
+                    <Card.Description>
+                        <ul>
+                            {entry.diagnosisCodes?.map((x) => {
+                                return (
+                                    <li key={x}>
+                                        {x} {showDetailsOfCode(x)}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </Card.Description>
+                </Card.Content>
+            </Card>
+        </div>
+    );
+};
+
+// const EntryDetails = ({ entry }: { entry: Entry }) => {
+//     console.log('props for entrydetails', entry);
+//     const [{ diagnosisDetail }] = useStateValue();
+
+//     console.log('diagnoseis', diagnosisDetail);
+//     const showDetailsOfCode = (x: string) => {
+//         if (diagnosisDetail) {
+//             return diagnosisDetail[x].name;
+//         }
+//     };
+
+//     return (
+//         <div>
+//             {entry.date} {entry.description}
+//             <ul>
+//                 {entry.diagnosisCodes?.map((x) => {
+//                     console.log('code is here', x);
+
+//                     return (
+//                         <li key={x}>
+//                             {x} {showDetailsOfCode(x)}
+//                         </li>
+//                     );
+//                 })}
+//             </ul>
+//         </div>
+//     );
+// };
+
+const EntryDetails = ({ entry }: { entry: Entry }) => {
+    switch (entry.type) {
+        case 'Hospital':
+            return <HospitalEntryDetails entry={entry} />;
+        case 'OccupationalHealthcare':
+            return <OccupationalHealthcareEntryDetails entry={entry} />;
+        case 'HealthCheck':
+            return <HealthCheckEntryDetails entry={entry} />;
+    }
 };
 
 export default EntryDetails;
