1ba5b70bb4cc329bdbdf764ab51985c59040c601
Lalit Ghimire <lalitghimire@gmail.com>
2022-06-07 17:15:47 +0300

    update api url

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Countries from "./components/Countries";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Countries from './components/Countries';
 
 const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [searchName, setSearchName] = useState("");
+    const [countries, setCountries] = useState([]);
+    const [searchName, setSearchName] = useState('');
 
-  // get countries from restcountries.eu
-  useEffect(() => {
-    //console.log("effect");
-    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
-      //console.log("promise fulfilled");
-      //console.log(response.data);
-      setCountries(response.data);
-    });
-  }, []);
+    // get countries from restcountries.eu
+    useEffect(() => {
+        //console.log("effect");
+        axios.get('https://restcountries.com/v2/all').then((response) => {
+            //console.log("promise fulfilled");
+            //console.log(response.data);
+            setCountries(response.data);
+        });
+    }, []);
 
-  // event handler for searchbox
-  const handleSearch = (event) => {
-    console.log(event.target.value);
-    setSearchName(event.target.value);
-  };
+    // event handler for searchbox
+    const handleSearch = (event) => {
+        console.log(event.target.value);
+        setSearchName(event.target.value);
+    };
 
-  // filter the countries based on search characters
-  const countriesList = countries.filter((country) =>
-    country.name.toLowerCase().includes(searchName.toLowerCase())
-  );
+    // filter the countries based on search characters
+    const countriesList = countries.filter((country) =>
+        country.name.toLowerCase().includes(searchName.toLowerCase())
+    );
 
-  return (
-    <div>
-      find countries <input value={searchName} onChange={handleSearch} />
-      <br />
-      {
-        <Countries
-          countriesList={countriesList}
-          searchName={searchName}
-          setSearchName={setSearchName}
-        />
-      }
-    </div>
-  );
+    return (
+        <div>
+            find countries <input value={searchName} onChange={handleSearch} />
+            <br />
+            {
+                <Countries
+                    countriesList={countriesList}
+                    searchName={searchName}
+                    setSearchName={setSearchName}
+                />
+            }
+        </div>
+    );
 };
 
 export default App;
--- part2/data-for-countries/src/components/Countries.js
+++ part2/data-for-countries/src/components/Countries.js
@@ -1,82 +1,80 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 // weather info component
 const ShowWeather = ({ capital }) => {
-  console.log("here capital is ", capital);
-  const [weather, setWeather] = useState([]);
+    console.log('here capital is ', capital);
+    const [weather, setWeather] = useState([]);
 
-  useEffect(() => {
-    //console.log("effect");
-    const api_key = process.env.REACT_APP_API_KEY;
-    axios
-      .get(
-        `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${capital}`
-      )
-      .then((response) => {
-        //console.log("promise fulfilled");
-        console.log("this is weather", response.data);
-        setWeather(response.data);
-      });
-  }, []);
+    useEffect(() => {
+        //console.log("effect");
+        const api_key = 'd28dca293a1f477994f165634211809';
+        axios
+            .get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${capital}`)
+            .then((response) => {
+                //console.log("promise fulfilled");
+                console.log('this is weather', response.data);
+                setWeather(response.data);
+            });
+    }, []);
 
-  return (
-    <div>
-      <h3>Weather in {capital}</h3>
-      <p>temperature: {weather.current.temp_c} Celcius</p>
-      <img src={weather.current.condition.icon} alt="image not available" />
-      <p>
-        wind: {weather.current.wind_kph} kmph direction{" "}
-        {weather.current.wind_dir}
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <h3>Weather in {capital}</h3>
+            {weather && <p>temperature: {weather.current.temp_c} Celcius</p>}
+            {weather && <img src={weather.current.condition.icon} alt='not available' />}
+            {weather && (
+                <p>
+                    wind: {weather.current.wind_kph} kmph direction {weather.current.wind_dir}
+                </p>
+            )}
+        </div>
+    );
 };
 
 // country details component
 const CountryDetail = ({ country }) => {
-  const name = country.name;
-  const capital = country.capital;
-  const population = country.population;
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>capital {capital} </p>
-      <p>population {population} </p>
-      <h3> Spoken languages </h3>
-      <ul>
-        {country.languages.map((c) => (
-          <li key={c.name}>{c.name}</li>
-        ))}{" "}
-      </ul>
-      <img src={country.flag} alt="flag" width="100" height="100" />
-      <ShowWeather capital={capital} />
-    </div>
-  );
+    const name = country.name;
+    const capital = country.capital;
+    const population = country.population;
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>capital {capital} </p>
+            <p>population {population} </p>
+            <h3> Spoken languages </h3>
+            <ul>
+                {country.languages.map((c) => (
+                    <li key={c.name}>{c.name}</li>
+                ))}{' '}
+            </ul>
+            <img src={country.flag} alt='flag' width='100' height='100' />
+            <ShowWeather capital={capital} />
+        </div>
+    );
 };
 
 // Countries component
 const Countries = ({ countriesList, searchName, setSearchName }) => {
-  const showDetail = (country) => {
-    setSearchName(country.name);
-  };
-  console.log("this is test", searchName);
-  if (searchName.length === 0) {
-    return <p>Type to search countries</p>;
-  } else if (countriesList.length > 10) {
-    return <p>Too many matches, specify another filter</p>;
-  } else if (countriesList.length > 1) {
-    return countriesList.map((country) => (
-      <p key={country.name}>
-        {country.name}{" "}
-        <button onClick={() => showDetail(country)}> show</button>
-      </p>
-    ));
-  } else if (countriesList.length === 1) {
-    //console.log(countriesList[0]);
-    return <CountryDetail country={countriesList[0]} />;
-  } else return <p>try again</p>;
+    const showDetail = (country) => {
+        setSearchName(country.name);
+    };
+    console.log('this is test', searchName);
+    if (searchName.length === 0) {
+        return <p>Type to search countries</p>;
+    } else if (countriesList.length > 10) {
+        return <p>Too many matches, specify another filter</p>;
+    } else if (countriesList.length > 1) {
+        return countriesList.map((country) => (
+            <p key={country.name}>
+                {country.name} <button onClick={() => showDetail(country)}> show</button>
+            </p>
+        ));
+    } else if (countriesList.length === 1) {
+        //console.log(countriesList[0]);
+        return <CountryDetail country={countriesList[0]} />;
+    } else return <p>try again</p>;
 };
 
 export default Countries;
